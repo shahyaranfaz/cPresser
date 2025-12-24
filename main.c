7eb581e1be2a510b69534930bbed7cba2baa1d85
Shahyar Anfaz <shahyar.anfaz@mail.utoronto.ca>
2025-12-23 22:48:46 -0500

    created skeleton classes

--- main.c
+++ main.c
@@ -4,11 +4,29 @@
 #define SEARCH_BUFFER 64000
 #define LOOKAHEAD_BUFFER 256
 
-typedef struct Token {
+typedef struct {
     uint16_t distance;
     uint8_t length;
     unsigned char symbol;
 } Token;
+
+typedef struct {
+    int distance;
+    int length;
+} Match;
+
+//FILE INPUT
+void read_file(char* file_name);
+
+//COMPRESSION
+Match find_longest_match();
+Token build_token();
+void tokenize();
+void encode();
+void write_file(char* file_name);
+
+//DECOMPRESSION
+void decode();
 
 int main(void) { return 0; }
 
