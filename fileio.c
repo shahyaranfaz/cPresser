098eda46dc54ded140760f7698435b7affd57918
Shahyar Anfaz <shahyar.anfaz@mail.utoronto.ca>
2025-12-26 15:36:41 -0500

    fixed formatting of code

--- fileio.c
+++ fileio.c
@@ -17,18 +17,14 @@ unsigned char *read_file(const char *filename, size_t *read_size) {
     }
 
     const long size = ftell(fp);
-    if (size < 0) {
-        fclose(fp);
-        return NULL;
-    }
 
-    fseek(fp, 0, SEEK_SET);
-
-    if (size == 0) {
+    if (size <= 0) {
         fclose(fp);
         *read_size = 0;
         return NULL;
     }
+
+    fseek(fp, 0, SEEK_SET);
 
     unsigned char *buffer = malloc(size);
     if (!buffer) {
@@ -41,11 +37,8 @@ unsigned char *read_file(const char *filename, size_t *read_size) {
 
     while (total_read < (size_t) size) {
         size_t to_read = (size_t)size - total_read;
-        if (to_read > CHUNK_SIZE)
-            to_read = CHUNK_SIZE;
-
+        to_read = to_read > CHUNK_SIZE ? CHUNK_SIZE : to_read;
         const size_t bytes = fread(buffer + total_read, 1, to_read, fp);
-
         if (bytes == 0) {
             if (ferror(fp)) {
                 free(buffer);
@@ -54,37 +47,29 @@ unsigned char *read_file(const char *filename, size_t *read_size) {
             }
             break;
         }
-
         total_read += bytes;
     }
-
     fclose(fp);
-
     if (total_read != (size_t) size) {
         free(buffer);
         return NULL;
     }
-
     *read_size = total_read;
     return buffer;
 }
 
 void write_file(const char *filename, unsigned char* buffer, size_t *write_size) {
     if (!buffer || !write_size || *write_size == 0) return;
-
     FILE *fp = fopen(filename, "wb");
     if (!fp) {
         perror("Error opening file");
         return;
     }
-
     size_t total_written = 0;
-
     while (total_written < *write_size) {
         size_t to_write = *write_size - total_written;
         if (to_write > CHUNK_SIZE)
             to_write = CHUNK_SIZE;
-
         const size_t bytes = fwrite(buffer + total_written, 1, to_write, fp);
         if (bytes == 0) {
             if (ferror(fp))
@@ -96,7 +81,6 @@ void write_file(const char *filename, unsigned char* buffer, size_t *write_size)
     if (total_written != *write_size) {
         fprintf(stderr, "Warning: wrote %zu of %zu bytes\n", total_written, *write_size);
     }
-
     fclose(fp);
     *write_size = total_written;
 }
